57adec0a7e1a50696f22ffcb058dc21aa1f9fb68
ting <manyuanhuahua90@gmail.com>
2022-09-08 00:10:41 -0700

    fix conflict

--- react-app/src/components/form/editActivity.js
+++ react-app/src/components/form/editActivity.js
@@ -267,7 +267,7 @@ function EditActivity(){
           </div>
 
           <div className='map-buttons'>
-              <button onClick={()=> displayRoute(markers[0].coords,markers[1].coords)}>Display</button>
+              <button type='button' onClick={()=> displayRoute(markers[0].coords,markers[1].coords)}>Display</button>
               <button type='submit'onClick={handleSubmit}>Update</button>
 
               <button onClick={hancleCancel}>Cancel</button>
