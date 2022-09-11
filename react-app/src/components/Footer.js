98e57126a8bf320eabf58d9828b1d5770659112f
ting <manyuanhuahua90@gmail.com>
2022-09-11 15:00:53 -0700

    footer added

---
+++ react-app/src/components/Footer.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import github from "../assets/git.png"
+import linkedin from "../assets/linkdin.png"
+
+
+
+function Footer() {
+    return (
+    <div className='footer-container'>
+
+            <div className='github'>
+                <a href='https://github.com/manyuanhuahua' >
+                <img src={github} alt='' className='git' style={{borderRadius:'50%',width:'30px', height:'30px',marginRight:'10px' }}/>
+                </a>
+                <p>Github</p>
+            </div>
+            <div className='linkedin'>
+            <a href='https://www.linkedin.com/in/tf-sde/' >
+            <img src={linkedin} alt='' className='linkedIn' style={{width:'30px', height:'30px',marginRight:'10px'}}/>
+            </a>
+            <p>LinkedIn</p>
+            </div>
+            {/* <div className='portfolio'>
+
+                <p>Portfolio</p>
+            </div> */}
+
+    </div>
+    )
+}
+
+export default Footer
--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -3,15 +3,16 @@ import { Link } from 'react-router-dom';
 
 import videoHome from "../assets/homeVideo.mp4"
 import "./style/homepage.css"
+import Footer from './Footer';
+
+
 
 const Main = () =>{
 
 
     return (
         <div className='main-container'>
-            <div className='loading'>
 
-            </div>
             <div className='overlay'></div>
 
                 <video src={videoHome} autoPlay loop muted playsInline className='bgvideo'/>
@@ -21,8 +22,7 @@ const Main = () =>{
                     <Link to={'/explore'} className='explore'>Exploring</Link>
                 </div>
                 <div className='footer'>
-                    <img className='github'></img>
-                    <img className='linkedin'></img>
+                    <Footer />
                 </div>
         </div>
     )
